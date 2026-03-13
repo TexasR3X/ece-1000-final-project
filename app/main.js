0044cded97706b670b62792b61a978ac11722122
Texas R3X <texasr3xphone@gmail.com>
2026-03-13 16:13:03 -0600

    Add  in ; Delete unused code

--- app/main.js
+++ app/main.js
@@ -1,13 +1,14 @@
 const { app, BrowserWindow } = require("electron/main");
+const path = require("path");
 
 // Define how to create a new window for the app
 function createWindow() {
-    const win = new BrowserWindow({
+    const window = new BrowserWindow({
         width: 800,
         height: 600
     });
 
-    win.loadFile("index.html");
+    window.loadFile(path.join(".", "frontend", "index.html"));
 }
 
 // Create a new window when the app opens
@@ -15,20 +16,3 @@ app.whenReady().then(createWindow);
 
 // Quite the app when every window closes (for every OS)
 app.on("window-all-closed", app.quit);
-
-
-// app.whenReady().then(() => {
-//     createWindow();
-
-//     app.on("activate", () => {
-//         if (BrowserWindow.getAllWindows().length === 0) {
-//             createWindow();
-//         }
-//     });
-// });
-
-// app.on("window-all-closed", () => {
-//     if (process.platform !== "darwin") {
-//         app.quit();
-//     }
-// });
