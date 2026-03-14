883172aa21268995edb2f8cbd22a8d5460d670e8
Texas R3X <texasr3xphone@gmail.com>
2026-03-13 22:49:51 -0600

    Handle log() preload function in main.js; Cause Chrome developer tools to be opened in the window

--- app/main.js
+++ app/main.js
@@ -1,18 +1,29 @@
-const { app, BrowserWindow } = require("electron/main");
+const { app, BrowserWindow, ipcMain } = require("electron/main");
 const path = require("path");
 
 // Define how to create a new window for the app
 function createWindow() {
     const window = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
     });
 
-    window.loadFile(path.join(".", "frontend", "index.html"));
+    // Create and open the window
+    window.loadFile(path.join(__dirname, "frontend", "index.html"));
+
+    // Open the Chrome developer tools in the window
+    window.webContents.openDevTools();
 }
 
 // Create a new window when the app opens
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    ipcMain.on("log", (_, ...args) => console.log(...args));
+
+    createWindow();
+});
 
 // Quite the app when every window closes (for every OS)
 app.on("window-all-closed", app.quit);
