5ec96da65a2f72b221b7065ef9544c3e73d3a8b1
Texas R3X <texasr3xphone@gmail.com>
2026-03-21 16:31:58 -0600

    Add buttons to <ControlPanel/>
    - Add button's styles
    - Add button's functionality with backendService

--- app/frontend/src/views/ControlPanel.vue
+++ app/frontend/src/views/ControlPanel.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
-    import backendService from '@/services/backendService';
+    import backendService from "@/services/backendService";
 </script>
 
 <template>
-    <strong>Control Panel</strong>
+    <div class="component-container">
+        <div class="grid-container">
+            <!-- Top Row -->
+            <div class="empty-grid-cell"></div>
+            <button class="up-btn" @click="backendService.setDriveState(`UP`)">Up</button>
+            <div class="empty-grid-cell"></div>
+            <!-- Middle Row -->
+            <button class="left-btn" @click="backendService.setDriveState(`LEFT`)">Left</button>
+            <button class="stop-btn" @click="backendService.setDriveState(`STOP`)">Stop</button>
+            <button class="right-btn" @click="backendService.setDriveState(`RIGHT`)">Right</button>
+            <!-- Bottom Row -->
+            <div class="empty-grid-cell"></div>
+            <button class="down-btn" @click="backendService.setDriveState(`DOWN`)">Down</button>
+            <div class="empty-grid-cell"></div>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .component-container {
+        margin-top: 100px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .grid-container {
+        width: 400px;
+        display: grid;
+        grid-template-columns: 3fr 3fr 3fr;
+        gap: 10px;
+    }
+
+    button {
+        font-size: 40px;
+        font-weight: bold;
+        background-color: #7bdcff;
+        padding: 10px;
+        border: 0 solid #000000;
+        border-radius: 20px;
+    }
+</style>
