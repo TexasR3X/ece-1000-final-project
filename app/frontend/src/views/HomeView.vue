8ca3a46b184506c36fce56e7305cafb89b97d377
Texas R3X <texasr3xphone@gmail.com>
2026-03-21 16:34:56 -0600

    Clean up code in HomeView.vue

--- app/frontend/src/views/HomeView.vue
+++ app/frontend/src/views/HomeView.vue
@@ -10,31 +10,24 @@
 </script>
 
 <template>
-    <div class="btn-container">
-        <button
-            id="connect-to-car-btn"
-            class="btn"
-            @click="handleClick"
-        >
-            Connect to Car
-        </button>
+    <div class="container">
+        <button @click="handleClick()">Connect to Car</button>
     </div>
 </template>
 
 <style scoped>
-    .btn-container {
+    .container {
         margin-top: 100px;
-        display: flex block;
+        display: flex;
         justify-content: center;
     }
 
-    .btn {
+    button {
         font-size: 40px;
         font-weight: bold;
         background-color: #7bdcff;
         padding: 30px;
         border: 0 solid #000000;
         border-radius: 20px;
-        cursor: pointer;
     }
 </style>
