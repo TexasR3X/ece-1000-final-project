35981b9feec15eeccb15274f5d4d25a16e59d053
Texas R3X <texasr3xphone@gmail.com>
2026-03-20 22:17:44 -0600

    Change frontend to use Vue.js; App still mostly Vue.js template code

--- app/frontend/renderer.js
+++
@@ -1,6 +0,0 @@
-const connectToCarBtn = document.getElementById("connect-to-car-btn");
-
-connectToCarBtn.addEventListener("click", () => {
-    backendApi.log("FRONTEND: Connecting to car.");
-    backendApi.connectToCar();
-});
