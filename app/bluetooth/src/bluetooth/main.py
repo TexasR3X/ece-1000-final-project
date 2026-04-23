ba2010f352bbab2167403569ec8d6e9b17d826bf
Texas R3X <texasr3xphone@gmail.com>
2026-04-22 19:04:42 -0600

    Start updating main.py to connect to car in more logical way

--- app/bluetooth/src/bluetooth/main.py
+++ app/bluetooth/src/bluetooth/main.py
@@ -1,17 +1,31 @@
 import asyncio
-import bleak
-import bleak.exc
-from src.bluetooth.node_api import send_specific_message, log_message
+from bleak import BleakScanner, BleakClient
+from bleak.exc import BleakBluetoothNotAvailableError
+from bleak.backends.service import BleakGATTService
+from bluetooth.main_process_api import send_stdout_message, log_message
 import sys
 import json
 
 DEVICE_NAME = "ArduinoCar"
 DRIVE_SERVICE_UUID = "678630d9-bbb4-4867-af2e-fb51af7097da"
 
+# async def main():
+#     for stdin in sys.stdin:
+#         # Parse the stdin
+#         message_object = json.loads(stdin)
+
+#         if message_object.message_type == "main-bluetooth--connect-to-car":
+#             await connect_to_car()
+#         else:
+#             log_message(f"Unknown message_type ({message_object.message_type})")
+
+#         # print(f"data: -->{parsed_stdin}<--")
+#         # log_message(f"TEMP data: {parsed_stdin}")
+
 async def main():
     try:
         log_message("Searching for car...")
-        devices = await bleak.BleakScanner.discover()
+        devices = await BleakScanner.discover()
 
         target_device = None
 
@@ -22,36 +36,42 @@ async def main():
                 target_device = device
                 break
         else:
-            send_specific_message("python-node--car-not-found")
+            send_stdout_message("bluetooth-main--car-not-found")
             return
 
         log_message("Connecting to car...")
 
-        async with bleak.BleakClient(target_device) as peripheral:
-            send_specific_message("python-node--connected-to-car")
+        async with BleakClient(target_device) as peripheral_device:
+            send_stdout_message("bluetooth-main--connected-to-car")
 
-            drive_service = peripheral.services.get_service(DRIVE_SERVICE_UUID)
+            drive_service = peripheral_device.services.get_service(DRIVE_SERVICE_UUID)
 
             if drive_service == None:
                 raise Exception("Car doesn't have the drive service.")
-            
+
             log_message(f"drive_service: {drive_service}")
 
-            # for json_message in sys.stdin:
-            #     data = json.loads(json_message)
-            #     log_message(f"data: {data}")
+            wait_for_stdin(drive_service)
 
-    except bleak.exc.BleakBluetoothNotAvailableError:
-        send_specific_message("python-node--bluetooth-off")
+    except BleakBluetoothNotAvailableError:
+        send_stdout_message("python-node--bluetooth-off")
         # log_message("Error: Bluetooth cannot be used. This device likely has it turned off.")
 
     except Exception as ex:
-        send_specific_message("python-node--unknown-error", str(ex))
+        send_stdout_message("python-node--unknown-error", str(ex))
         # log_message(f"Error: {ex}")
 
+def wait_for_stdin(drive_service: BleakGATTService):
+    for stdin in sys.stdin:
+        # Parse the stdin
+        message_object = json.loads(stdin)
+
+        if message_object.message_type == "main-bluetooth--change-drive-state":
+            log_message("Change the drive state!")
+        else:
+            log_message(f"Unknown message_type ({message_object.message_type})")
+
+        log_message(f"TEMP data: {message_object}")
+
 if __name__ == "__main__":
-    log_message("Start Python Process")
-
     asyncio.run(main())
-
-    log_message("End Python Process")
